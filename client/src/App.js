f9c6327df1c138f3c6b30e1733fc32999f1d1c3f
FredSchuck <fredschuck@gmail.com>
2024-03-06 20:18:28 -0500

    Completed basic home page layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 
 const App = () => {
   return (
-    <h1>Welcome to Sakura Market</h1>
-  )
-}
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Sakura Market</h1>
+          <Outlet />
+        </Container>
+        </main>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
