132929f50fc1a1295594a29b291f8eb8369ca7b4
FredSchuck <fredschuck@gmail.com>
2024-03-08 16:37:24 -0500

    Moved routes Changed routing statements and added custom error handling

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 4001;
+
+connectDB();
 
 const app = express();
 
@@ -12,13 +16,8 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running at http://${host}:${port}/`));
