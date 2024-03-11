4dcaf97fc47b3e337ef571ba441377500d9a2b8c
FredSchuck <fredschuck@gmail.com>
2024-03-11 18:55:23 -0400

    Added middleware for parsing JSON, URL encoded data, and cookies

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorHandler.js';
 
 const host = process.env.HOST || 'localhost';
@@ -12,11 +14,20 @@ connectDB();
 
 const app = express();
 
+// Middleware for parsing JSON and URLencoded form data
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+// Middleware for parsing cookies
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
