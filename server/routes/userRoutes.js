f1b3747132e88f3da8d09cc2394b2e221a536cf7
FredSchuck <fredschuck@gmail.com>
2024-03-11 18:54:23 -0400

    Created user routes

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { 
+    authUser, 
+    registerUser, 
+    logoutUser, 
+    getUserProfile, 
+    updateUserProfile, 
+    getUsers, 
+    getUserById, 
+    deleteUser, 
+    updateUser 
+ } from '../controllers/userController.js';
+import { protect, isAdmin } from '../middleware/authMiddleware.js';
+
+const router = express.Router();
+
+router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
+router.post('/logout', logoutUser);
+router.post('/login', authUser);
+router
+    .route('/profile')
+    .get(protect, getUserProfile)
+    .put(protect, updateUserProfile);
+
+router
+    .route('/:id')
+    .get(protect, isAdmin, getUserById)
+    .put(protect, isAdmin, updateUser)
+    .delete(protect, isAdmin, deleteUser);
+
+
+export default router;
