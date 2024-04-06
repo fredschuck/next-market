f7f69c5e7bc080b97c080f9c747741c26fd450e0
Fred Schuck <fredschuck@gmail.com>
2024-04-06 11:14:39 -0400

    Added createProduct endpoint

--- client/src/slices/productsApiSlice.js
+++ client/src/slices/productsApiSlice.js
@@ -2,21 +2,33 @@ import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        getProducts: builder.query({ // 'getProducts' is the name of the query endpoint
-            query: () => ({
-                url: PRODUCTS_URL,
-            }),
-            keepUnusedDataFor: 5,
-        }),
-        getProductDetails: builder.query({
-            query: (productId) => ({
-                url: `${PRODUCTS_URL}/${productId}`,
-            }),
-            keepUnusedDataFor: 5,
-        })
-    })
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      // 'getProducts' is the name of the query endpoint
+      query: () => ({
+        url: PRODUCTS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    getProductDetails: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    createProduct: builder.mutation({
+      query: (product) => ({
+        url: PRODUCTS_URL,
+        method: "POST",
+      }),
+      invalidatesTags: ["Product"], // stops it from being cached
+    }),
+  }),
 });
 
 // Naming Convention: 'use' + [Query Endpoint Name] + 'Query'
-export const { useGetProductsQuery, useGetProductDetailsQuery } = productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetProductDetailsQuery,
+  useCreateProductMutation,
+} = productsApiSlice;
