7485bc798513152bb32297b1a8d0c6e83e3e7ac2
Fred Schuck <fredschuck@gmail.com>
2024-04-06 11:04:34 -0400

    Added AdminRoute, OrderListScreen, ProductListScreen

--- client/src/index.js
+++ client/src/index.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter, 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
- } from 'react-router-dom';
-import { PayPalScriptProvider } from '@paypal/react-paypal-js';
-import { Provider } from 'react-redux';
-import store from './store';
+  RouterProvider,
+} from "react-router-dom";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { Provider } from "react-redux";
+import store from "./store";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './assets/styles/bootstrap.custom.css';
-import './assets/styles/index.css';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PrivateRoute from './components/PrivateRoute';
-import HomeScreen from './pages/HomeScreen';
-import ProductScreen from './pages/ProductScreen';
-import CartScreen from './pages/CartScreen';
-import LoginScreen from './pages/LoginScreen';
-import RegisterScreen from './pages/RegisterScreen';
-import ShippingScreen from './pages/ShippingScreen';
-import PaymentScreen from './pages/PaymentScreen';
-import PlaceOrderScreen from './pages/PlaceOrderScreen';
-import OrderScreen from './pages/OrderScreen';
-import ProfileScreen from './pages/ProfileScreen';
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import PrivateRoute from "./components/PrivateRoute";
+import AdminRoute from "./components/AdminRoute";
+import HomeScreen from "./pages/HomeScreen";
+import ProductScreen from "./pages/ProductScreen";
+import CartScreen from "./pages/CartScreen";
+import LoginScreen from "./pages/LoginScreen";
+import RegisterScreen from "./pages/RegisterScreen";
+import ShippingScreen from "./pages/ShippingScreen";
+import PaymentScreen from "./pages/PaymentScreen";
+import PlaceOrderScreen from "./pages/PlaceOrderScreen";
+import OrderScreen from "./pages/OrderScreen";
+import ProfileScreen from "./pages/ProfileScreen";
+import OrderListScreen from "./pages/admin/OrderListScreen";
+import ProductListScreen from "./pages/admin/ProductListScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,23 +39,28 @@ const router = createBrowserRouter(
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
       {/* Registered Users */}
-      <Route path='' element={<PrivateRoute />} >
+      <Route path="" element={<PrivateRoute />}>
         <Route path="/shipping" element={<ShippingScreen />} />
         <Route path="/payment" element={<PaymentScreen />} />
         <Route path="/placeorder" element={<PlaceOrderScreen />} />
         <Route path="/orders/:id" element={<OrderScreen />} />
         <Route path="/profile" element={<ProfileScreen />} />
       </Route>
+      {/* Admin Users */}
+      <Route path="" element={<AdminRoute />}>
+        <Route path="/admin/orderlist" element={<OrderListScreen />} />
+        <Route path="/admin/productlist" element={<ProductListScreen />} />
+      </Route>
     </Route>
   )
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <PayPalScriptProvider deferLoading={ true }>
-        <RouterProvider router={ router } />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
       </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
