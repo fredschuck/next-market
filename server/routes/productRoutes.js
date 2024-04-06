11122dff80ebe27b63676e8479ed369c899d3c95
Fred Schuck <fredschuck@gmail.com>
2024-04-06 12:16:44 -0400

    Added route for updateProduct

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,11 +3,12 @@ import {
   getProduct,
   getProductById,
   createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
 const router = express.Router();
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProduct).post(protect, isAdmin, createProduct);
-router.route("/:id").get(getProductById);
+router.route("/:id").get(getProductById).put(protect, isAdmin, updateProduct);
 
 export default router;
