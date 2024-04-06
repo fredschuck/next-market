6e0153efd86983aa6ea8f7c88d093f88430fce3a
Fred Schuck <fredschuck@gmail.com>
2024-04-06 11:20:23 -0400

    Added route to createProduct and imported middleware for authentication and admin authorization

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,10 +1,13 @@
-import express from 'express';
-import { getProduct, getProductById } from '../controllers/productController.js';
+import express from "express";
+import {
+  getProduct,
+  getProductById,
+  createProduct,
+} from "../controllers/productController.js";
 const router = express.Router();
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
-
-router.route('/').get(getProduct);
-
-router.route('/:id').get(getProductById);
+router.route("/").get(getProduct).post(protect, isAdmin, createProduct);
+router.route("/:id").get(getProductById);
 
 export default router;
