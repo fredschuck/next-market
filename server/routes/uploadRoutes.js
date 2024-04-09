e98c830afd915b39c6a06121392c3fcd7f4a56c5
FredSchuck <fredschuck@gmail.com>
2024-04-09 14:39:03 -0400

    Created uploadRoute file and contents

---
+++ server/routes/uploadRoutes.js
@@ -0,0 +1,44 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    // cb is callback
+    cb(null, "uploads/"); // null is for errors
+  },
+  filename(req, file, cb) {
+    cb(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Images only!"), false);
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  res.send({
+    message: "Image uploaded",
+    path: `/${req.file.path}`,
+  });
+});
+
+export default router;
