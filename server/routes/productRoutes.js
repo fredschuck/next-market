404e80999c5e20f59f52e9a4cc60d54352dbb3b2
FredSchuck <fredschuck@gmail.com>
2024-04-09 14:38:37 -0400

    Added delete route

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,14 +1,19 @@
 import express from "express";
+const router = express.Router();
 import {
   getProduct,
   getProductById,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
-const router = express.Router();
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProduct).post(protect, isAdmin, createProduct);
-router.route("/:id").get(getProductById).put(protect, isAdmin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, isAdmin, updateProduct)
+  .delete(protect, isAdmin, deleteProduct);
 
 export default router;
