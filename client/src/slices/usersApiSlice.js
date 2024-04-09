d5068132af3741a64e757b2efd3ff8bf4765b98c
FredSchuck <fredschuck@gmail.com>
2024-04-09 18:56:19 -0400

    Added getUsers, deleteUser, getUserDetails, and updateUser endpoints

--- client/src/slices/usersApiSlice.js
+++ client/src/slices/usersApiSlice.js
@@ -2,36 +2,74 @@ import { USERS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        login: builder.mutation({ // 'login' is the name of the mutation endpoint
-            query: (data) => ({ // 'query' is a function that returns the options for the fetch request & 'data' is the payload obtained from the form
-                url: `${USERS_URL}/auth`, // 'url' is the URL to send the request to
-                method: 'POST',
-                body: data
-            }),
-        }),
-        register: builder.mutation({
-            query: (data) => ({
-                url: `${USERS_URL}`,
-                method: 'POST',
-                body: data
-            })
-        }),
-        logout: builder.mutation({
-            query: () => ({
-                url: `${USERS_URL}/logout`,
-                method: 'POST'
-            })
-        }),
-        profile: builder.mutation({
-            query: (data) => ({
-                url: `${USERS_URL}/profile`,
-                method: 'PUT',
-                body: data
-            })
-        })
-    })
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      // 'login' is the name of the mutation endpoint
+      query: (data) => ({
+        // 'query' is a function that returns the options for the fetch request & 'data' is the payload obtained from the form
+        url: `${USERS_URL}/auth`, // 'url' is the URL to send the request to
+        method: "POST",
+        body: data,
+      }),
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: "POST",
+      }),
+    }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: `${USERS_URL}`,
+        method: "GET",
+      }),
+      providesTags: ["Users"],
+      keepUnusedDataFor: 5,
+    }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+        method: "DELETE",
+      }),
+    }),
+    getUserDetails: builder.query({
+      query: (userId) => ({
+        url: `${USERS_URL}/${userId}`,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+    updateUser: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/${data.userId}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: ["Users"],
+    }),
+  }),
 });
 
-// Naming Convention: 'use' + [Query Endpoint Name] + 'Mutation'
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation, useProfileMutation } = usersApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} = usersApiSlice;
