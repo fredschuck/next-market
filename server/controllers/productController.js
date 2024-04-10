d7a03844561d8a39edf1f549a3c589ee2118023a
FredSchuck <fredschuck@gmail.com>
2024-04-10 12:35:44 -0400

    Created logic for getProducts function for obtaining all products, searching, and pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -4,9 +4,25 @@ import Product from "../models/productModel.js";
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-const getProduct = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = process.env.PAGINATION_LIMIT;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc    Fetch single product
@@ -124,11 +140,21 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.status(200).json(products);
+});
+
 export {
-  getProduct,
+  getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
   createProductReview,
+  getTopProducts,
 };
