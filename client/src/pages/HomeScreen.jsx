2751b14360bacfc93217a2b51d04f1ef51c4c0e9
FredSchuck <fredschuck@gmail.com>
2024-04-10 20:55:17 -0400

    Commented out Product Carousel for testing deployment

--- client/src/pages/HomeScreen.jsx
+++ client/src/pages/HomeScreen.jsx
@@ -17,7 +17,8 @@ const HomeScreen = () => {
   return (
     <>
       {!keyword ? (
-        <ProductCarousel />
+        // <ProductCarousel />
+        <p>Product Carousel</p>
       ) : (
         <Link to="/" className="btn btn-light mb-4">
           Go Back
